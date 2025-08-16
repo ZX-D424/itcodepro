9e23fdb85683af47668f472be013817b1135b535
xuexin_419 <305930712@qq.com>
2025-08-16 12:03:10 +0800

    feat:排行榜新页面开发

--- code-ui/src/views/projectRank.vue
+++ code-ui/src/views/projectRank.vue
@@ -115,38 +115,119 @@
       </div>
 
     </div>
+
+    <!-- 二级开始-->
     <div class="rank_list">
-      <div class="rank-list-item">
-        <img src=""/>
-        <p>
-          <span>HTMl</span>
-          排名：<span>1</span>
-          得分：<span>100</span>
-          总分数：<span>100</span>
-          总人数：<span>100</span>
-        </p>
-      </div>
-      <div class="rank-list-item">
-        <img src=""/>
-        <p>
-          <span>CSS</span>
-          排名：<span>1</span>
-          得分：<span>100</span>
-          总分数：<span>100</span>
-          总人数：<span>100</span>
-        </p>
+      <div class="rank-list-item fixclear">
+        <div class="rank-item-left">
+          <div class="left-aside" >
+            <img src="../assets/homePage/images/nebulas.png"/>
+            <p >
+              <b>HTMl</b><br>
+              排名：<span>1</span><br>
+              得分：<span>100</span>
+            </p>
+          </div>
+          <div class="left-aside" >
+            <img src="../assets/homePage/images/nebulas.png"/>
+            <p >
+              <b>CSS</b><br>
+              排名：<span>1</span><br>
+              得分：<span>100</span>
+            </p>
+          </div>
+          <div class="left-aside" >
+            <img src="../assets/homePage/images/nebulas.png"/>
+            <p >
+              <b>JavaScript</b><br>
+              排名：<span>1</span><br>
+              得分：<span>100</span>
+            </p>
+          </div>
+          <div class="left-aside" >
+            <img src="../assets/homePage/images/nebulas.png"/>
+            <p >
+              <b>Jquery</b><br>
+              排名：<span>1</span><br>
+              得分：<span>100</span>
+            </p>
+          </div>
+          <div class="left-aside" >
+            <img src="../assets/homePage/images/nebulas.png"/>
+            <p >
+              <b>React.js</b><br>
+              排名：<span>1</span><br>
+              得分：<span>100</span>
+            </p>
+          </div>
+          <div class="left-aside" >
+            <img src="../assets/homePage/images/nebulas.png"/>
+            <p >
+              <b>Vue3.js</b><br>
+              排名：<span>1</span><br>
+              得分：<span>100</span>
+            </p>
+          </div>
+        </div>
+        <div class="rank-item-right">
+
+          <div class="left-aside-content1">
+            <h3 title="排行_TOP_100">HTML基础闯关排行榜</h3>
+            <hr>
+            <div class="ranking">
+              <table width="95%" align="center">
+                <tbody>
+                <tr class="ranking-list">
+                  <td>排名</td>
+                  <td>昵称</td>
+                  <td>评分</td>
+                </tr>
+                <tr v-for="(item, index) in 10" :key="index">
+                  <td>
+                    <img v-if="index === 0" src="https://www.w3cschool.cn/statics/images/codecamp/first.png" alt="第一名">
+                    <img v-else-if="index === 1" src="https://www.w3cschool.cn/statics/images/codecamp/second.png" alt="第二名">
+                    <img v-else-if="index === 2" src="https://www.w3cschool.cn/statics/images/codecamp/third.png" alt="第三名">
+                    <span v-else style="color: #333c46">{{ index + 1 }}</span>
+                  </td>
+                  <td>大队长</td>
+                  <td>100分</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+
+
+          <div class="left-aside-content1">
+            <h3 title="排行_TOP_100">Web开发者证书申请榜</h3>
+            <hr>
+            <div class="ranking">
+              <table width="95%" align="center">
+                <tbody>
+                <tr class="ranking-list">
+                  <td>排名</td>
+                  <td>昵称</td>
+                  <td>评分</td>
+                </tr>
+                <tr v-for="(item, index) in 10" :key="index">
+                  <td>
+                    <img v-if="index === 0" src="https://www.w3cschool.cn/statics/images/codecamp/first.png" alt="第一名">
+                    <img v-else-if="index === 1" src="https://www.w3cschool.cn/statics/images/codecamp/second.png" alt="第二名">
+                    <img v-else-if="index === 2" src="https://www.w3cschool.cn/statics/images/codecamp/third.png" alt="第三名">
+                    <span v-else style="color: #333c46">{{ index + 1 }}</span>
+                  </td>
+                  <td>大队长</td>
+                  <td>100分</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </div>
       </div>
 
-      <div class="rank-list-item">
-        <img src=""/>
-        <p>
-          <span>JavaScript</span>
-          排名：<span>1</span>
-          得分：<span>100</span>
-          总分数：<span>100</span>
-          总人数：<span>100</span>
-        </p>
-      </div>
     </div>
 
   </div>
